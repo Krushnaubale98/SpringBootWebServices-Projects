f910d51eff4d2e0d2d99d85cfb3fd58cb2add329
a2z <a2z@DESKTOP-ES6Q0S4>
2023-10-05 18:28:34 +0530

    SpringRestWebServices-Project

---
+++ SpringBootRest10-MiniProject01-CRUDOperations/src/main/java/com/nt/SpringBootRest10MiniProject01CrudOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRest10MiniProject01CrudOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRest10MiniProject01CrudOperationsApplication.class, args);
+	}
+
+}
